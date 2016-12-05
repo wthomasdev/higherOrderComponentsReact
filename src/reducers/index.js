e5dd16204ffb328c5ac7c19b903f43af59f81045
Will Thomas <wlthomas001@gmail.com>
2016-12-04 18:17:33 -0700

    set up false authenticate action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import authnticationReducer from './authentication'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  authenticated: authnticationReducer
 });
 
 export default rootReducer;
